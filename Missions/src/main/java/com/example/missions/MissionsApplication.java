e12a155f692f0e66b6244c5d9ef6abd01c40118c
Anes-boubaker <anes.boubaker@esprit.tn>
2025-04-08 09:20:38 +0100

    added eureka client

--- Missions/src/main/java/com/example/missions/MissionsApplication.java
+++ Missions/src/main/java/com/example/missions/MissionsApplication.java
@@ -2,8 +2,10 @@ package com.example.missions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MissionsApplication {
 
 	public static void main(String[] args) {
